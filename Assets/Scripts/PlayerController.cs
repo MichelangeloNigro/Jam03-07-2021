5ef15f32e0a37de097885c10602fa5ae738bc403
Kristi Rrapaj <74373101+kristirrapaj@users.noreply.github.com>
2021-07-03 12:33:11 +0200

    spaceship rotates around planet

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[HideInInspector] public float timeCounter = 0;
+    public CircleCollider2D planetCollider;
+    public Vector2 centerPlanet;
+    public float offset;
+
+    public void Start()
+    {
+        centerPlanet = planetCollider.bounds.center;
+    }
+
+    public void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        float x = Mathf.Cos(timeCounter);
+        float y = Mathf.Sin(timeCounter);
+        //float z = Mathf.Cos(timeCounter);
+
+        transform.position = centerPlanet + new Vector2 (x*offset, y*offset);
+    }
+}
