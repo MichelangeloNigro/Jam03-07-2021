cc04711d2b66be9fd99ae63c11cca32c8bcfd36a
Xomrac <morettimarco2759@gmail.com>
2021-07-03 16:06:32 +0200

    added camera mover

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] GameObject tileSample;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+
+    public void ChangeLevel()
+    {
+        StartCoroutine(Slide());
+    }
+    public IEnumerator Slide()
+    {
+        var XPos = transform.position.x;
+        while (transform.position.x < transform.position.x + tileSample.GetComponent<BoxCollider2D>().size.x)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
